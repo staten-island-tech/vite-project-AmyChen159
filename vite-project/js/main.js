7a0c8629cb4519ac13f55939ea24b3902fb1085e
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2023-11-22 09:10:45 -0500

    asd

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -5,6 +5,22 @@ document.querySelector('#app').innerHTML = `
   <h1>Hello Vite???</h1>
 `;
 
+function createCard(card) {
+  document.querySelector('#container').insertAdjacentElement(
+    `afterbegin`,
+    <div class="gallery">
+      <h2>${card.name}</h2> 
+      <img src="${card.icon}"alt=""></img>
+      <h3>${card.weapon} | ${card.element}</h3>
+      <h3>${card.region}</h3>
+      <h3>${card.rarity}</h3>
+    </div>
+    ) 
+}
+
+characters.forEach()
+
+
 console.log(characters);
 
 //npm install aos --save 
