83f92adfd1d8132b67fd535986a7403e7d57e3bb
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2023-11-29 02:31:17 -0500

    huhhh

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,10 +1,9 @@
 import '../styles/style.css';
 import { characters } from './array';
 
-document.querySelector('#app').innerHTML = `
+/* document.querySelector('#app').innerHTML = `
   <h1>Hello Vite???</h1>
-`;
-//domselecors
+`; */
 
 const DOMselectors = {
   theme: document.getElementById("theme"),
@@ -37,19 +36,6 @@ function remove() {
   document.querySelectorAll('.gallery').forEach((card) => card.remove())
 }
 
-/* const filters = {
-  fivestarcharacters: function() {
-    DOMselectors.fivestar.addEventListener("click", function(event) {
-      event.preventDefault()
-      remove()
-      const fivestars = characters.filter(fivestar => fivestar.rarity.includes("5 Star"))
-      createCard(fivestars)
-    }
-    )
-  }
-}
-*/
-
 let buttons = document.querySelectorAll(".button")
 buttons.forEach((button) => button.addEventListener("click",
 function() {
@@ -68,14 +54,47 @@ function() {
   createCard(array2)
 }))
 
-/* 
-DOMselectors.fivestar.addEventListener("click", function() {
-  remove()
-  characters.filter((fivestar) => fivestar.rarity.includes("5 Star").forEach(fivestar => createCard(fivestar)))
+DOMselectors.theme.addEventListener("click", function() {
+  if (document.body.classList.contains("blue")) {
+    document.body.classList.add("beige")
+    document.body.classList.remove("blue")
+  }
+  else {
+    document.body.classList.add("blue")
+    document.body.classList.remove("beige")
+  } 
 })
+
+/* 
+const filters = {
+  fivestarcharacters: function() {
+    DOMselectors.fivestar.addEventListener("click", function(event) {
+      event.preventDefault()
+      remove()
+      const fivestars = characters.filter(fivestar => fivestar.rarity.includes("5 Star"))
+      createCard(fivestars)
+    }
+    )
+  }
+}
 
 DOMselectors.fourstar.addEventListener("click", function() {
   remove()
   characters.filter((fourstar) => fourstar.rarity.includes("4 Star").forEach(fourstar => createCard(fourstar)))
 }) 
+
+DOMselectors.mondstadt.addEventListener("click", function() {
+  remove()
+  characters.filter((mondstadt) => mondstadt.region.includes("Mondstadt").forEach(mondstadt => createCard(mondstadt)))
+})
+
+DOMselectors.liyue.addEventListener("click", function() {
+  remove()
+  characters.filter((liyue) => liyue.region.includes("Liyue").forEach(liyue => createCard(liyue)))
+})
+
+DOMselectors.inazuma.addEventListener("click", function() {
+  remove()
+  characters.filter((inazuma) => inazuma.region.includes("Inazuma").forEach(inazuma => createCard(inazuma)))
+})
 */
